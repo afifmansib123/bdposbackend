2d85de9d7fcc9b9baad324d2486e504e13a4bccc
afifmansib123 <78995381+afifmansib123@users.noreply.github.com>
2024-09-24 17:42:06 +0700

    Update tokens.js-1

--- routes/tokens.js
+++ routes/tokens.js
@@ -13,7 +13,7 @@ router.post('/generate-token', (req, res) => {
     }
     const token = crypto.randomBytes(16).toString('hex'); // Generate a unique token
     tokens[token] = tableId; // Store token and tableId
-    const qrUrl = `https://bdposbackend.vercel.app/handle-redirect?token=${token}&tableId=${tableId}`;
+    const qrUrl = `https://bdposfrontend-customer.vercel.app/handle-redirect?token=${token}&tableId=${tableId}`;
     res.json({ qrUrl });
 });
 
